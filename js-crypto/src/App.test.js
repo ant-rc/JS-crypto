5c7826c678000f38614e3adb7fb3f767f380909b
jihad <jihad.douhipro@gmail.com>
2024-10-25 16:38:37 +0200

    app.test.js test de fonctionnalite

--- js-crypto/src/App.test.js
+++ js-crypto/src/App.test.js
@@ -1,8 +1,58 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+// test('renders learn react link', () => {
+//   render(<App />);
+//   // const linkElement = screen.getByText(/learn react/i);
+//   const buttons = screen.getByRole('button', {name: /learn react/i});
+//   expect(buttons).toBeInTheDocument();
+// });
+
+
+test('entrez message', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  const input = screen.getByPlaceholderText(/entrez votre message/i); //cherche l'input
+  expect(input).toBeInTheDocument(); //trouve l'input
+  const message = 'message'; //declare un msg
+  fireEvent.change(input, { target: { value: 'message' } }); // simule l'evenement de changement
+  expect(input.value).toBe(message); //on s'attend a trouver le msg declarer
+})
+
+test('cryptage note', () => {
+  render(<App />);
+  const buttons = screen.getByRole('button', {name: /Crypter la note/i});
+  expect(buttons).toBeInTheDocument();
+})
+
+test('affiche msg crypte', () => {
+  render(<App/>);
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//EXEMPLE
+// function calculate(a) {
+//   // logic 
+//   return 4
+// }
+
+// test.only("my logic", () => {
+//   expect(calculate(2)).toBe(4);
+// })
